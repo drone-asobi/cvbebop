5d141c00b793ff0973e5277ee0b3bea8765d52d7
Yoon SeungYong <forcom.kr@gmail.com>
2016-10-28 16:10:42 +0900

    Deleted unnecessary files

--- cppDrone/main.cpp
+++ cppDrone/main.cpp
@@ -18,13 +18,12 @@
 #include "bebopCommand.h"
 
 
-int main(void)
-{
+void process_bebop() {
 	bebopCommand bebop;
-	cv::Mat img;	
+	cv::Mat img;
 	bebop.takeOff();
-	
-	while (1){
+
+	while (1) {
 		img = bebop.getImage();
 		if (img.empty()) continue;
 		cv::imshow("image", img);
@@ -41,5 +40,47 @@ int main(void)
 		bebop.move(x, y, z, r);
 	}
 	bebop.landing();
+}
+
+void process_opencv() {
+	// ref: http://qiita.com/vs4sh/items/4a9ce178f1b2fd26ea30
+	cv::VideoCapture cap(0);//デバイスのオープン
+							//cap.open(0);//こっちでも良い．
+
+	if (!cap.isOpened())//カメラデバイスが正常にオープンしたか確認．
+	{
+		//読み込みに失敗したときの処理
+		return;
+	}
+
+	while (1)//無限ループ
+	{
+		cv::Mat frame;
+		cap >> frame; // get a new frame from camera
+
+					  //
+					  //取得したフレーム画像に対して，クレースケール変換や2値化などの処理を書き込む．
+					  //
+
+		cv::imshow("window", frame);//画像を表示．
+
+		int key = cv::waitKey(1);
+		if (key == 113)//qボタンが押されたとき
+		{
+			break;//whileループから抜ける．
+		}
+		else if (key == 115)//sが押されたとき
+		{
+			//フレーム画像を保存する．
+			cv::imwrite("img.png", frame);
+		}
+	}
+	cv::destroyAllWindows();
+}
+
+int main(void)
+{
+	process_opencv();
+	// process_bebop();
 	return 0;
 }
