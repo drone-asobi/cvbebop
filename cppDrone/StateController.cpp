43314b34f5430764ee9e97388a395318f5548c65
Yoon SeungYong <forcom.kr@gmail.com>
2016-12-05 02:35:57 +0900

    Renamed BEBOP2_STATE_MACHINE to StateController

--- cppDrone/StateController.cpp
+++ cppDrone/StateController.cpp
@@ -1,23 +1,24 @@
-#define TAG "state_machine"
+#define TAG "StateController"
 
-#include "state_machine.h"
+#include "StateController.h"
 
-void BEBOP2_STATE_MACHINE::process_bebop2_state(BEBOP2_STATE_PARAMETER* arg)
+void StateController::processState(STATE_PARAMETER* arg)
 {
 	static ULONGLONG startedTick = 0;
 
-	auto state = get_bebop2_state();
+	auto state = getState();
 	switch (state)
 	{
-	case BEBOP2_STATE_EMERGENCY:
+	case STATE_EMERGENCY:
 	{
 		ARSAL_PRINT(ARSAL_PRINT_FATAL, TAG, "State[Emergency]: Stop!!!!!!!!!!!!!");
 		deviceController->aRDrone3->sendPilotingEmergency(deviceController->aRDrone3);
+		setState(STATE_FINISHED);
 	}
 	break;
-	case BEBOP2_STATE_START:
+	case STATE_START:
 	{
-		auto param = static_cast<BEBOP2_STATE_PARAMETER_START*>(arg);
+		auto param = static_cast<STATE_PARAMETER_START*>(arg);
 		if (param == nullptr)
 		{
 			ARSAL_PRINT(ARSAL_PRINT_WARNING, TAG, "State[Start]: No parameters!");
@@ -27,13 +28,13 @@ void BEBOP2_STATE_MACHINE::process_bebop2_state(BEBOP2_STATE_PARAMETER* arg)
 		if (param->connected)
 		{
 			ARSAL_PRINT(ARSAL_PRINT_INFO, TAG, "State[Start => Ready]: Waiting for a take-off command...");
-			set_bebop2_state(BEBOP2_STATE_READY);
+			setState(STATE_READY);
 		}
 	}
 	break;
-	case BEBOP2_STATE_READY:
+	case STATE_READY:
 	{
-		auto param = static_cast<BEBOP2_STATE_PARAMETER_READY*>(arg);
+		auto param = static_cast<STATE_PARAMETER_READY*>(arg);
 		if (param == nullptr)
 		{
 			ARSAL_PRINT(ARSAL_PRINT_WARNING, TAG, "State[Ready]: No parameters!");
@@ -42,42 +43,42 @@ void BEBOP2_STATE_MACHINE::process_bebop2_state(BEBOP2_STATE_PARAMETER* arg)
 
 		switch (param->command)
 		{
-		case BEBOP2_STATE_PARAMETER_READY::BEBOP2_STATE_PARAMETER_READY_COMMAND_NONE:
+		case STATE_PARAMETER_READY::COMMAND_NONE:
 		{
 			// Ready. do nothing.
 		}
 		break;
-		case BEBOP2_STATE_PARAMETER_READY::BEBOP2_STATE_PARAMETER_READY_COMMAND_TAKEOFF:
+		case STATE_PARAMETER_READY::COMMAND_TAKEOFF:
 		{
 			ARSAL_PRINT(ARSAL_PRINT_INFO, TAG, "State[Ready => TakingOff]: Try to take off.");
 			deviceController->aRDrone3->sendPilotingTakeOff(deviceController->aRDrone3);
-			set_bebop2_state(BEBOP2_STATE_TAKINGOFF);
+			setState(STATE_TAKINGOFF);
 		}
 		break;
-		case BEBOP2_STATE_PARAMETER_READY::BEBOP2_STATE_PARAMETER_READY_COMMAND_DISCONNECT:
+		case STATE_PARAMETER_READY::COMMAND_DISCONNECT:
 		{
 			ARSAL_PRINT(ARSAL_PRINT_INFO, TAG, "State[Ready => Finished]: Try to disconnect.");
-			set_bebop2_state(BEBOP2_STATE_FINISHED);
+			setState(STATE_FINISHED);
 		}
 		break;
 		default: break;
 		}
 	}
 	break;
-	case BEBOP2_STATE_TAKINGOFF:
+	case STATE_TAKINGOFF:
 	{
-		auto param = static_cast<BEBOP2_STATE_PARAMETER_TAKINGOFF*>(arg);
+		auto param = static_cast<STATE_PARAMETER_TAKINGOFF*>(arg);
 		if (param == nullptr)
 		{
 			ARSAL_PRINT(ARSAL_PRINT_WARNING, TAG, "State[TakingOff]: No parameters!");
 			break;
 		}
 
-		if (GetTickCount64() - param->takenOffTick >= BEBOP2_TAKING_OFF_WAIT_TICK)
+		if (GetTickCount64() - param->takenOffTick >= TAKING_OFF_WAIT_TICK)
 		{
 			ARSAL_PRINT(ARSAL_PRINT_INFO, TAG, "State[TakingOff => Hovering]: Completed taking off.");
 			deviceController->aRDrone3->setPilotingPCMD(deviceController->aRDrone3, 0, 0, 0, 0, 0, 0);
-			set_bebop2_state(BEBOP2_STATE_HOVERING);
+			setState(STATE_HOVERING);
 		}
 		else
 		{
@@ -86,9 +87,9 @@ void BEBOP2_STATE_MACHINE::process_bebop2_state(BEBOP2_STATE_PARAMETER* arg)
 		}
 	}
 	break;
-	case BEBOP2_STATE_HOVERING:
+	case STATE_HOVERING:
 	{
-		auto param = static_cast<BEBOP2_STATE_PARAMETER_HOVERING*>(arg);
+		auto param = static_cast<STATE_PARAMETER_HOVERING*>(arg);
 		if (param == nullptr)
 		{
 			ARSAL_PRINT(ARSAL_PRINT_WARNING, TAG, "State[Hovering]: No parameters!");
@@ -97,24 +98,24 @@ void BEBOP2_STATE_MACHINE::process_bebop2_state(BEBOP2_STATE_PARAMETER* arg)
 
 		switch (param->command)
 		{
-		case BEBOP2_STATE_PARAMETER_HOVERING::BEBOP2_STATE_PARAMETER_HOVERING_COMMAND_NONE:
+		case STATE_PARAMETER_HOVERING::COMMAND_NONE:
 		{
 			// waiting for commands...
 			// ... and no move.
 			deviceController->aRDrone3->setPilotingPCMD(deviceController->aRDrone3, 0, 0, 0, 0, 0, 0);
 		}
 		break;
-		case BEBOP2_STATE_PARAMETER_HOVERING::BEBOP2_STATE_PARAMETER_HOVERING_COMMAND_SEARCH:
+		case STATE_PARAMETER_HOVERING::COMMAND_SEARCH:
 		{
 			ARSAL_PRINT(ARSAL_PRINT_INFO, TAG, "State[Hovering => Searching]: Start to search people...");
-			set_bebop2_state(BEBOP2_STATE_SEARCHING);
+			setState(STATE_SEARCHING);
 		}
 		break;
-		case BEBOP2_STATE_PARAMETER_HOVERING::BEBOP2_STATE_PARAMETER_HOVERING_COMMAND_LAND:
+		case STATE_PARAMETER_HOVERING::COMMAND_LAND:
 		{
 			ARSAL_PRINT(ARSAL_PRINT_INFO, TAG, "State[Hovering => Landing]: Try to land.");
 			deviceController->aRDrone3->sendPilotingLanding(deviceController->aRDrone3);
-			set_bebop2_state(BEBOP2_STATE_SEARCHING);
+			setState(STATE_LANDING);
 		}
 		break;
 		default:
@@ -124,9 +125,9 @@ void BEBOP2_STATE_MACHINE::process_bebop2_state(BEBOP2_STATE_PARAMETER* arg)
 		}
 	}
 	break;
-	case BEBOP2_STATE_SEARCHING:
+	case STATE_SEARCHING:
 	{
-		auto param = static_cast<BEBOP2_STATE_PARAMETER_SEARCHING*>(arg);
+		auto param = static_cast<STATE_PARAMETER_SEARCHING*>(arg);
 		if (param == nullptr)
 		{
 			ARSAL_PRINT(ARSAL_PRINT_WARNING, TAG, "State[Searching]: No parameters!");
@@ -137,14 +138,14 @@ void BEBOP2_STATE_MACHINE::process_bebop2_state(BEBOP2_STATE_PARAMETER* arg)
 		{
 			ARSAL_PRINT(ARSAL_PRINT_INFO, TAG, "State[Searching => Tracking]: Found a person! Try to track.");
 			deviceController->aRDrone3->setPilotingPCMD(deviceController->aRDrone3, 0, 0, 0, 0, 0, 0);
-			set_bebop2_state(BEBOP2_STATE_TRACKING);
+			setState(STATE_TRACKING);
 		}
 		else
 		{
-			if (GetTickCount64() - param->startedTick >= BEBOP2_SEARCHING_WAIT_TICK)
+			if (GetTickCount64() - param->startedTick >= SEARCHING_WAIT_TICK)
 			{
 				ARSAL_PRINT(ARSAL_PRINT_INFO, TAG, "State[Searching => Hovering]: No more people. Finish searching.");
-				set_bebop2_state(BEBOP2_STATE_HOVERING);
+				setState(STATE_HOVERING);
 			}
 			else
 			{
@@ -154,9 +155,9 @@ void BEBOP2_STATE_MACHINE::process_bebop2_state(BEBOP2_STATE_PARAMETER* arg)
 		}
 	}
 	break;
-	case BEBOP2_STATE_TRACKING:
+	case STATE_TRACKING:
 	{
-		auto param = static_cast<BEBOP2_STATE_PARAMETER_TRACKING*>(arg);
+		auto param = static_cast<STATE_PARAMETER_TRACKING*>(arg);
 		if (param == nullptr)
 		{
 			ARSAL_PRINT(ARSAL_PRINT_WARNING, TAG, "State[Tracking]: No parameters!");
@@ -165,24 +166,24 @@ void BEBOP2_STATE_MACHINE::process_bebop2_state(BEBOP2_STATE_PARAMETER* arg)
 
 		switch (param->status)
 		{
-		case BEBOP2_STATE_PARAMETER_TRACKING::BEBOP2_STATE_PARAMETER_TRACKING_STATUS_NONE:
+		case STATE_PARAMETER_TRACKING::STATUS_NONE:
 		{
 			ARSAL_PRINT(ARSAL_PRINT_WARNING, TAG, "State[Tracking]: Status is None!");
 			deviceController->aRDrone3->setPilotingPCMD(deviceController->aRDrone3, 0, 0, 0, 0, 0, 0);
 		}
 		break;
-		case BEBOP2_STATE_PARAMETER_TRACKING::BEBOP2_STATE_PARAMETER_TRACKING_STATUS_FOUND:
+		case STATE_PARAMETER_TRACKING::STATUS_FOUND:
 		{
 			switch (param->direction)
 			{
-			case BEBOP2_STATE_PARAMETER_TRACKING::BEBOP2_STATE_PARAMETER_TRACKING_DIRECTION_NONE:
+			case STATE_PARAMETER_TRACKING::DIRECTION_NONE:
 			{
 				// do nothing
 				ARSAL_PRINT(ARSAL_PRINT_DEBUG, TAG, "State[Tracking]: Found, but wait.");
 				deviceController->aRDrone3->setPilotingPCMD(deviceController->aRDrone3, 0, 0, 0, 0, 0, 0);
 			}
 			break;
-			case BEBOP2_STATE_PARAMETER_TRACKING::BEBOP2_STATE_PARAMETER_TRACKING_DIRECTION_FORWARD:
+			case STATE_PARAMETER_TRACKING::DIRECTION_FORWARD:
 			{
 				// go forward
 				ARSAL_PRINT(ARSAL_PRINT_DEBUG, TAG, "State[Tracking]: Found, go forward");
@@ -190,14 +191,14 @@ void BEBOP2_STATE_MACHINE::process_bebop2_state(BEBOP2_STATE_PARAMETER* arg)
 				deviceController->aRDrone3->setPilotingPCMDPitch(deviceController->aRDrone3, 100);
 			}
 			break;
-			case BEBOP2_STATE_PARAMETER_TRACKING::BEBOP2_STATE_PARAMETER_TRACKING_DIRECTION_LEFT:
+			case STATE_PARAMETER_TRACKING::DIRECTION_LEFT:
 			{
 				// turn left
 				ARSAL_PRINT(ARSAL_PRINT_DEBUG, TAG, "State[Tracking]: Found, turn left");
 				deviceController->aRDrone3->setPilotingPCMDYaw(deviceController->aRDrone3, -100);
 			}
 			break;
-			case BEBOP2_STATE_PARAMETER_TRACKING::BEBOP2_STATE_PARAMETER_TRACKING_DIRECTION_RIGHT:
+			case STATE_PARAMETER_TRACKING::DIRECTION_RIGHT:
 			{
 				// turn right
 				ARSAL_PRINT(ARSAL_PRINT_DEBUG, TAG, "State[Tracking]: Found, turn right");
@@ -208,28 +209,28 @@ void BEBOP2_STATE_MACHINE::process_bebop2_state(BEBOP2_STATE_PARAMETER* arg)
 			}
 		}
 		break;
-		case BEBOP2_STATE_PARAMETER_TRACKING::BEBOP2_STATE_PARAMETER_TRACKING_STATUS_MISSED:
+		case STATE_PARAMETER_TRACKING::STATUS_MISSED:
 		{
-			ARSAL_PRINT(ARSAL_PRINT_INFO, TAG, "State[Tracking => MISSING]: Missed a person!");
+			ARSAL_PRINT(ARSAL_PRINT_INFO, TAG, "State[Tracking => Missing]: Missed a person!");
 			deviceController->aRDrone3->setPilotingPCMD(deviceController->aRDrone3, 0, 0, 0, 0, 0, 0);
-			set_bebop2_state(BEBOP2_STATE_MISSING);
+			setState(STATE_MISSING);
 		}
 		break;
-		case BEBOP2_STATE_PARAMETER_TRACKING::BEBOP2_STATE_PARAMETER_TRACKING_STATUS_CAPTURED:
+		case STATE_PARAMETER_TRACKING::STATUS_CAPTURED:
 		{
 			ARSAL_PRINT(ARSAL_PRINT_INFO, TAG, "State[Tracking => Searching]: Captured a person! Back to searching");
 			// TODO: ’ÇÕ‚ªI‚í‚Á‚½‚çƒhƒ[ƒ“‚ð‚Ç‚Ì‚æ‚¤‚É“®‚©‚·‚©Œˆ‚ß‚é
 			deviceController->aRDrone3->setPilotingPCMD(deviceController->aRDrone3, 0, 0, 0, 0, 0, 0);
-			set_bebop2_state(BEBOP2_STATE_SEARCHING);
+			setState(STATE_SEARCHING);
 		}
 		break;
 		default: break;
 		}
 	}
 	break;
-	case BEBOP2_STATE_MISSING:
+	case STATE_MISSING:
 	{
-		auto param = static_cast<BEBOP2_STATE_PARAMETER_MISSING*>(arg);
+		auto param = static_cast<STATE_PARAMETER_MISSING*>(arg);
 		if (param == nullptr)
 		{
 			ARSAL_PRINT(ARSAL_PRINT_WARNING, TAG, "State[Missing]: No parameters!");
@@ -240,14 +241,14 @@ void BEBOP2_STATE_MACHINE::process_bebop2_state(BEBOP2_STATE_PARAMETER* arg)
 		if (param->found)
 		{
 			ARSAL_PRINT(ARSAL_PRINT_INFO, TAG, "State[Missing => Tracking]: Found the person. Back to tracking");
-			set_bebop2_state(BEBOP2_STATE_TRACKING);
+			setState(STATE_TRACKING);
 		}
 		else
 		{
-			if (GetTickCount64() - param->missedTick >= BEBOP2_MISSING_WAIT_TICK)
+			if (GetTickCount64() - param->missedTick >= MISSING_WAIT_TICK)
 			{
 				ARSAL_PRINT(ARSAL_PRINT_INFO, TAG, "State[Missing => Searching]: Failed to find the person. Back to searching");
-				set_bebop2_state(BEBOP2_STATE_MISSING);
+				setState(STATE_SEARCHING);
 			}
 			else
 			{
@@ -256,19 +257,19 @@ void BEBOP2_STATE_MACHINE::process_bebop2_state(BEBOP2_STATE_PARAMETER* arg)
 		}
 	}
 	break;
-	case BEBOP2_STATE_LANDING:
+	case STATE_LANDING:
 	{
-		auto param = static_cast<BEBOP2_STATE_PARAMETER_LANDING*>(arg);
+		auto param = static_cast<STATE_PARAMETER_LANDING*>(arg);
 		if (param == nullptr)
 		{
 			ARSAL_PRINT(ARSAL_PRINT_WARNING, TAG, "State[Landing]: No parameters!");
 			break;
 		}
 
-		if (GetTickCount64() - param->landedTick >= BEBOP2_LANDING_WAIT_TICK)
+		if (GetTickCount64() - param->landedTick >= LANDING_WAIT_TICK)
 		{
 			ARSAL_PRINT(ARSAL_PRINT_INFO, TAG, "State[Landing => Ready]: Completed landing. Get ready.");
-			set_bebop2_state(BEBOP2_STATE_READY);
+			setState(STATE_READY);
 		}
 		else
 		{
@@ -276,9 +277,9 @@ void BEBOP2_STATE_MACHINE::process_bebop2_state(BEBOP2_STATE_PARAMETER* arg)
 		}
 	}
 	break;
-	case BEBOP2_STATE_FINISHED:
+	case STATE_FINISHED:
 	{
-		auto param = static_cast<BEBOP2_STATE_PARAMETER_FINISHED*>(arg);
+		auto param = static_cast<STATE_PARAMETER_FINISHED*>(arg);
 		if (param == nullptr)
 		{
 			ARSAL_PRINT(ARSAL_PRINT_WARNING, TAG, "State[Finished]: No parameters!");
